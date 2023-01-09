88759dd94f1e14125f2e811a34ea9c70a43861fc
Spyglezz <spyglezz@outlook.com>
2023-01-09 12:02:47 +0100

    controller etc

---
+++ controller.js
@@ -0,0 +1,13 @@
+import { contacts } from "./data.js";
+
+function getContacts(req, res) {
+  res.status(200).json(contacts);
+}
+
+function getContact(req, res) {
+  const id = req.params.id;
+  contacts.find(getContact());
+  res.status(200).json({ id });
+}
+
+export { getContacts, getContact };
