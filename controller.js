419d89e168b2b4654359550e63fbf713f043f956
Spyglezz <spyglezz@outlook.com>
2023-01-10 09:00:42 +0100

    ajout de la route par id avec test reussi

--- controller.js
+++ controller.js
@@ -6,8 +6,12 @@ function getContacts(req, res) {
 
 function getContact(req, res) {
   const id = req.params.id;
-  contacts.find(getContact());
-  res.status(200).json({ id });
+  const contact = contacts.find((c) => c.id === id);
+  if (contact === undefined) {
+    res.status(200).json("Contact not found");
+  } else {
+    res.status(200).json(contact);
+  }
 }
 
 export { getContacts, getContact };
